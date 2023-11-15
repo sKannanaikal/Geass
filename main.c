4d966389c4eb5218808d8cb7075388313cda328c
Sean <sean@ghostblood.student.rit.edu>
2023-11-15 13:45:39 -0500

    added analysis and parsing of elf header and segments

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <elf.h>
 #include <string.h>
+#include <dirent.h>
+#include <ftw.h>
 
 int checkifElf(Elf64_Ehdr elfHeader){
     unsigned char *fileMagicNumbers = elfHeader.e_ident;
@@ -14,37 +16,46 @@ int checkifElf(Elf64_Ehdr elfHeader){
     return 1;
 }
 
-Elf64_Ehdr extractHeader(FILE *filePointer){
+int walkDirectory(const char *filename, const struct stat *statptr, int fileflags){
+    FILE *filePointer;
     Elf64_Ehdr elfHeader;
-
-    filePointer = fopen("target", "rb");
-    if(filePointer == NULL){
-        return NULL;
-    }
+    int result;
+    long unsigned int originalEntryPoint;
+    
+    filePointer = fopen(filename, "rb");
     fread(&elfHeader, sizeof(elfHeader), 1, filePointer);
 
-    return elfHeader;
-}
+    result = checkifElf(elfHeader);
+    if(result == 0 && elfHeader.e_type == 3 && strstr(filename, "geass") == NULL){
+        printf("[*] %s is an Executable ELF file!\n", filename);
+        printf("\tEntry Point: 0x%lx\n", elfHeader.e_entry);
 
-void filesInDirectory(){
+        originalEntryPoint = elfHeader.e_entry;
 
+        Elf64_Phdr elfSegment;
+        while(elfSegment.p_type != 4){
+            fread(&elfSegment, sizeof(elfSegment), 1, filePointer);
+        }
+
+        elfSegment.p_type = 1;
+        if(elfSegment.p_type == 1){
+            printf("\tPT_NOTE segment Updated to PT_LOAD segment\n");
+        }
+    }
+
+    return 0;
 }
 
 int main(int argc, char *argv[]){
-    Elf64_Ehdr elfHeader;
-    FILE *filePointer;
+    char directoryName[strlen(argv[1])];
     int result;
 
-    elfHeader = extractHeader(filePointer);
+    //verify argc is two
 
-    unsigned int entryPoint = elfHeader.e_entry;
+    memcpy(directoryName, argv[1], strlen(argv[1]));
+    printf("[*] Scanning Directory: %s\n", directoryName);
 
-    printf("The Entry Point is 0x%x\n", entryPoint);
-
-    result = checkifElf(elfHeader);
-    if(result == 0){
-        printf("This is an elf file!\n");
-    }
+    result = ftw(directoryName, walkDirectory, 2);
 
     return 0;
 }
